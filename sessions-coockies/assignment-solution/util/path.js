4212060dc40b33d4f880f3918b904bfa93f6e58c
Artem Mahey <artemmagej@gmail.com>
2023-07-13 16:43:22 +0300

    learn: cookies and sessions

--- sessions-coockies/assignment-solution/util/path.js
+++
@@ -1,3 +0,0 @@
-const path = require('sessions-coockies/assignment-solution/util/path');
-
-module.exports = path.dirname(process.mainModule.filename);
---
+++ sessions-cookies/assignment-solution/util/path.js
@@ -0,0 +1,3 @@
+const path = require('sessions-cookies/assignment-solution/util/path');
+
+module.exports = path.dirname(process.mainModule.filename);
